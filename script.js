cd225dd132b33804ef85e3c3d12a15b49473b643
Enforsix <christian.gulkoder@gmail.com>
2024-12-16 21:24:58 +0100

    menue eddeting

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello!');
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
